c2533172f4cae479e5693684770049abe05080dc
fabio henrique <henri8274@gmail.com>
2024-12-10 16:18:56 -0300

    put-link-getway

--- guiasuplemento/src/Componentes/Button.jsx
+++ guiasuplemento/src/Componentes/Button.jsx
@@ -7,9 +7,13 @@ export default function Button(){
             <h2>Garanta já o seu!</h2>
             <p>Não perca tempo! Invista na sua saúde e transforme seus resultados hoje mesmo. Clique no botão abaixo e adquira o Guia Prático de Suplementação agora.</p>
             <div className='Botao'>
-            <button>COMPRAR AGORA</button>
+            <button onClick={()=>ConnectGetway()}>COMPRAR AGORA</button>
             </div>
          </div>
         </div>
     )
+
+    function ConnectGetway(){
+        window.location.href="https://techouseofc.pay.yampi.com.br/r/MRYBDDHPFP"
+    }
 }
