d62541176fc1848633ba7639e37b362a04d0965f
fabio henrique <henri8274@gmail.com>
2024-12-12 16:01:17 -0300

    upDate-all

--- guiasuplemento/src/Componentes/Head.jsx
+++ guiasuplemento/src/Componentes/Head.jsx
@@ -10,9 +10,15 @@ export default function Head() {
                     <img src="/MonneyOn.png" alt=''></img>
                 </div>
                 <div className='texto'>
-                   <p>Quer saber como gerar uma renda extra ou até mesmo transformar sua vida financeira trabalhando online? Este eBook é a chave para começar a ganhar dinheiro pela internet de maneira prática, sem precisar sair de casa!</p>
+                    <p>Quer saber como gerar uma renda extra ou até mesmo transformar sua vida financeira trabalhando online? Este eBook é a chave para começar a ganhar dinheiro pela internet de maneira prática, sem precisar sair de casa!</p>
                 </div>
-               
+                <h3>Por que este eBook é perfeito para você?</h3>
+                <p>Fácil de Seguir: Métodos claros, diretos e fáceis de aplicar.<br/><br/>
+                    Sem Complicação: Não precisa de habilidades técnicas avançadas para começar.<br/><br/>
+                    Estratégias Comprovadas: As três formas mais rentáveis e eficazes para começar a ganhar dinheiro agora.<br/><br/>
+                    Liberdade de Trabalho: Aprenda como montar um fluxo de trabalho online e ganhar no seu tempo.
+                </p>
+
             </div>
         </div>
     )
