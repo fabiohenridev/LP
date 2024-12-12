fec130b07c66f96815fe9d6f79a603e46cda2880
fabio henrique <henri8274@gmail.com>
2024-12-12 15:43:06 -0300

    upDate-all

--- guiasuplemento/src/Componentes/Button.jsx
+++ guiasuplemento/src/Componentes/Button.jsx
@@ -2,15 +2,18 @@ import './Button.css';
 
 export default function Button(){
     return(
-        <div>
+        <div>   
+            
          <div className='Button'>
             <h2>Garanta já o seu!</h2>
-            <h3>APENAS $1,99</h3>
-            <p>Não perca tempo! Invista na sua saúde e transforme seus resultados hoje mesmo. Clique no botão abaixo e adquira o Guia Prático de Suplementação agora.</p>
+            <h3>APENAS $9,90</h3>
+            <p>Adquira o Seu eBook Agora e Comece a Mudar Sua Vida Financeira!
+            Por apenas R$9,90, você terá acesso ao passo a passo das três melhores formas de ganhar dinheiro na internet.</p>
+         
             <div className='Botao'>
-            <button onClick={()=>ConnectGetway()}>COMPRAR AGORA</button>
+                <button target="blank_" onClick={()=>ConnectGetway()}>COMPRAR AGORA</button>
             </div>
-         </div>
+          </div>
         </div>
     )
 
