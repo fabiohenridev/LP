1ad71cbe3378b2b6e2d933bb237434232198c00e
fabio henrique <henri8274@gmail.com>
2024-12-27 16:37:46 -0300

    update-buttom

--- guiasuplemento/src/Componentes/Button.jsx
+++ guiasuplemento/src/Componentes/Button.jsx
@@ -1,25 +1,51 @@
+import { useState } from 'react';
 import './Button.css';
 
 
-export default function Button(){
-    return(
-        <div>   
-            
-         <div className='Button'>
-            <h2>Garanta já o seu!</h2>
-            <h3 className='Promo'>De $29,90 por</h3>
-            <h3>APENAS $4,99</h3>
-            <p>Adquira o Seu eBook Agora e Comece a Mudar Sua Vida Financeira!
-            Por apenas R$4,99, você terá acesso ao passo a passo das três melhores formas de ganhar dinheiro na internet.</p>
-         
-            <div className='Botao'>
-                <button target="blank_" onClick={()=>ConnectGetway()}>COMPRAR AGORA</button>
+export default function Button() {
+
+
+    function pulsarr(){
+
+        let frase = document.getElementById('pulsante');
+       frase.style.fontSize = '16px';
+        setTimeout(pulsante, 500);
+
+    }
+
+
+    function pulsante() {
+
+        let frase = document.getElementById('pulsante');
+          frase.style.fontSize = '17px'
+        setTimeout(pulsarr, 500);
+      
+    }
+
+
+setTimeout(pulsante, 500);
+
+    return (
+        <div>
+
+            <div className='Button'>
+                <h2>Garanta já o seu!</h2>
+                <h3 className='Promo'>De $29,90 por</h3>
+                <div className='pulsante'>
+                <h3 id='pulsante'>APENAS $4,99</h3>
+                </div>
+                <p className='textBottom'>Adquira o Seu eBook Agora e Comece a Mudar Sua Vida Financeira!
+                    Por apenas R$4,99, você terá acesso ao passo a passo das três melhores formas de ganhar dinheiro na internet.</p>
+
+                <div className='Botao'>
+                    <button target="blank_" onClick={() => ConnectGetway()}>COMPRAR AGORA</button>
+                </div>
             </div>
-          </div>
         </div>
     )
 
-    function ConnectGetway(){
-        window.location.href="https://techouseofc.pay.yampi.com.br/r/9ICECVUDD3"
+    function ConnectGetway() {
+        window.location.href = "https://techouseofc.pay.yampi.com.br/r/9ICECVUDD3"
     }
+
 }
