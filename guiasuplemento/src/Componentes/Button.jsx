84415fbb79864f5f43d5482ce7b7419c13d77611
fabio henrique <henri8274@gmail.com>
2025-01-06 13:24:42 -0300

    update-site

--- guiasuplemento/src/Componentes/Button.jsx
+++ guiasuplemento/src/Componentes/Button.jsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from 'react';
 import './Button.css';
+import { use } from 'react';
 
 
 export default function Button() {
+ 
+const [Font, setFont] = useState('20px');
 
-    const [tamButton, setTamButton] = useState(200);
-  
-    // Tamanho Botao alternando
+useState(()=>{
 
-    useEffect(()=>{
+const interval = setInterval(()=>{
 
-     const interval = setInterval(()=>{
-
-      setTamButton((prevTam) => (prevTam==200? 190 : 200))
-
-     },500)
+setFont((prevFont)=> (prevFont == '20px' ? '18px' : '20px'))
 
 
 
-        return () => clearInterval(interval)
+}, 500)
 
-    }, [])
+
+
+return () => clearInterval(interval)
+
+})
 
     return (
         <div>
@@ -28,17 +29,14 @@ export default function Button() {
             <div className='Button'>
                 <h2 className='GarantaJa'>Garanta já o seu!</h2>
                 <h3 className='Promo'>De $29,90 por</h3>
-                <h3 className='Apenas499'>APENAS $4,99</h3>
+                <div className='Apenas'>
+                <h3 style={{fontSize: Font, position:'relative', transition: `font-size 0.5s ease-in-out`, display: 'inline-block'}} className='Apenas499'>APENAS $4,99</h3>
+                </div>
                 <p className='textBottom'>Adquira o Seu eBook Agora e Comece a Mudar Sua Vida Financeira!
                     Por apenas R$4,99, você terá acesso ao passo a passo das três melhores formas de ganhar dinheiro na internet.</p>
 
                 <div className='Botao'>
-                    <button style={{
-
-                         border: tamButton, transition: `width 0.5s ease-in-out` 
-
-
-                    }} target="blank_" onClick={() => ConnectGetway()}>COMPRAR AGORA</button>
+                    <button  target="blank_" onClick={() => ConnectGetway()}>COMPRAR AGORA</button>
                 </div>
             </div>
         </div>
