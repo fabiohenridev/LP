a4bd17c64aecb288c015beded90d9d4a53630fa6
fabio henrique <henri8274@gmail.com>
2025-01-06 13:30:47 -0300

    update-site

--- guiasuplemento/src/Componentes/Button.jsx
+++ guiasuplemento/src/Componentes/Button.jsx
@@ -4,24 +4,24 @@ import { use } from 'react';
 
 
 export default function Button() {
- 
-const [Font, setFont] = useState('20px');
 
-useState(()=>{
+    const [Font, setFont] = useState('20px');
 
-const interval = setInterval(()=>{
+    useState(() => {
 
-setFont((prevFont)=> (prevFont == '20px' ? '18px' : '20px'))
+        const interval = setInterval(() => {
 
-
-
-}, 500)
+            setFont((prevFont) => (prevFont == '20px' ? '18px' : '20px'))
 
 
 
-return () => clearInterval(interval)
+        }, 500)
 
-})
+
+
+        return () => clearInterval(interval)
+
+    })
 
     return (
         <div>
@@ -30,13 +30,13 @@ return () => clearInterval(interval)
                 <h2 className='GarantaJa'>Garanta já o seu!</h2>
                 <h3 className='Promo'>De $29,90 por</h3>
                 <div className='Apenas'>
-                <h3 style={{fontSize: Font, position:'relative', transition: `font-size 0.5s ease-in-out`, display: 'inline-block'}} className='Apenas499'>APENAS $4,99</h3>
+                    <h3 style={{ fontSize: Font, position: 'relative', transition: `font-size 0.5s ease-in-out`, display: 'inline-block' }} className='Apenas499'>APENAS $4,99</h3>
                 </div>
                 <p className='textBottom'>Adquira o Seu eBook Agora e Comece a Mudar Sua Vida Financeira!
                     Por apenas R$4,99, você terá acesso ao passo a passo das três melhores formas de ganhar dinheiro na internet.</p>
 
                 <div className='Botao'>
-                    <button  target="blank_" onClick={() => ConnectGetway()}>COMPRAR AGORA</button>
+                    <button target="blank_" onClick={() => ConnectGetway()}>COMPRAR AGORA</button>
                 </div>
             </div>
         </div>
