6dd5f2fb882c6a0a89c146213a9796c2f884e4c9
fabio henrique <henri8274@gmail.com>
2025-01-08 14:53:11 -0300

    update-site

--- guiasuplemento/src/Componentes/Head.jsx
+++ guiasuplemento/src/Componentes/Head.jsx
@@ -5,30 +5,43 @@ export default function Head() {
     const [corOlho, setCorOlho] = useState('Red'); // Estado para controlar a cor
     const [view, setView] = useState(5);
     const [corTop, setCorTop] = useState(' rgba(62, 62, 62, 0.355)');
+    const [menuAberto, setMenuAberto] = useState(false);
 
-    useEffect(()=>{
+    // função para abrir menu
 
-      const interval = setInterval(()=>{
+    
+const Whatsapp = () =>{
+    window.location.href="https://wa.me/5584991562754?text=Ol%C3%A1,%20Gostaria%20de%20saber%20mais%20sobre%20o%20e-book...";
+}
+
+    const AbrirMenu = () => {
+        setMenuAberto(!menuAberto);
+    }
+
+    useEffect(() => {
+
+        const interval = setInterval(() => {
 
 
-        setCorTop((prevTop)=> (prevTop == 'rgba(62, 62, 62, 0.355)') ? 'rgba(84, 84, 84, 0.35)' : 'rgba(62, 62, 62, 0.355)' )
+            setCorTop((prevTop) => (prevTop == 'rgba(62, 62, 62, 0.355)') ? 'rgba(84, 84, 84, 0.35)' : 'rgba(62, 62, 62, 0.355)')
 
-      }, 500);
-     
+        }, 500);
 
-      return () => clearInterval(interval);
+
+        return () => clearInterval(interval);
 
     }, [])
 
-useState(()=>{
+    useState(() => {
 
-const interval = setTimeout(()=>{
+        const interval = setTimeout(() => {
 
-setView(3)
+            setView(3)
 
-return () => clearInterval(interval);
+            return () => clearInterval(interval);
 
-},7000)}, [])
+        }, 7000)
+    }, [])
 
 
     // Alterna a cor a cada 2 segundos
@@ -41,19 +54,46 @@ return () => clearInterval(interval);
         return () => clearInterval(interval);
     }, []);
 
+
     return (
         <div>
             <div className='DivEyeEye'>
                 <div className='DivEye'>
 
 
-                    <p style={{backgroundColor: corTop, transition: 'background 0.5s ease-in-out'}} ><i
+                    <p style={{ backgroundColor: corTop, transition: 'background 0.5s ease-in-out' }} ><i
                         id='Eye'
                         className='bi bi-eye-fill Eye'
                         style={{ color: corOlho }}
                     ></i>{view} Pessoas finalizando compra...</p>
 
                 </div>
+
+                {menuAberto && (
+
+
+                    <div className='Menu'>
+                        <div className='MenuDuvida'>
+                            <h5 className='Duvida'>DÚVIDAS?</h5>
+                            <div className='BotaoDuvida'>
+                                <button onClick={AbrirMenu} className='Close'><i class="bi bi-x-lg "></i></button>
+                            </div>
+                        </div>
+                        <div className='DescricaoDuvida'>
+                            <div className='ImgDuvida'>
+                                <img className='Img' src='./man.png'></img>
+                            </div>
+                            <div className='ConteudoDuvida'>
+                                <p>Olá, tudo bem!? Me chamo Fabio Lima. Se você tem dúvidas em relação ao E-book, pode entrar em contato com a gente pelo nosso whatsapp.</p>
+                            </div>
+
+                        </div>
+                        <button onClick={Whatsapp} className='whatsappD'><i class="bi bi-whatsapp whats"><span className='FaleComAgente'>IR PARA WHATSAPP</span></i></button>
+                    </div>
+
+
+                )}
+                <div className='DivMenu'><i onClick={AbrirMenu} class="bi bi-list whatsapp"></i></div>
             </div>
             <div className='Head'>
 
@@ -78,12 +118,12 @@ return () => clearInterval(interval);
                 </div>
                 <h2>Por que este eBook é perfeito para você?</h2>
                 <div className='Video'>
-                     
-                     <video  width={300} loop autoPlay muted>
 
-                         <source className='Videoo' src='/videoAnimated.mp4'></source>
+                    <video width={300} loop autoPlay muted>
 
-                     </video>
+                        <source className='Videoo' src='/videoAnimated.mp4'></source>
+
+                    </video>
 
                 </div>
                 <p>
