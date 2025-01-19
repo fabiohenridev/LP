4d3dcd2f1147678c7e047f1f863f891fb83dbe78
fabio henrique <henri8274@gmail.com>
2025-01-19 11:38:33 -0300

    update

--- guiasuplemento/src/Componentes/Head.jsx
+++ guiasuplemento/src/Componentes/Head.jsx
@@ -12,7 +12,7 @@ export default function Head() {
 
     const interval = setInterval(()=>{
 
-     setBackgroundX((prevBack)=> prevBack === 'rgba(0, 0, 0, 0.2)' ? 'rgba(255, 255, 255, 0.09)': 'rgba(0, 0, 0, 0.2)');
+     setBackgroundX((prevBack)=> prevBack === 'rgba(0, 0, 0, 0.2)' ? 'rgba(255, 255, 255, 0.34)': 'rgba(0, 0, 0, 0.2)');
 
     },1000)
 
